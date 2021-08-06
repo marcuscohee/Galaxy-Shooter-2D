647212aadd57cead50e376d0119a975449d0b03c
Marcus Cohee <82741496+marcuscohee@users.noreply.github.com>
2021-08-06 16:50:40 -0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{ 
+    [SerializeField] private float _speed = 3.5f;  
+    [SerializeField] private GameObject _laserPrefab;
+
+
+
+
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+
+    void Update()
+    {
+        CalculateMovement();
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
+
+    }
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        //movement
+        transform.Translate(_speed * Time.deltaTime * direction);
+        
+        //vertical player bounds
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        //horizontal player bounds
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+    }
+
+}
+
