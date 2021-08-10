46330c4b872104ffd83f9196ace7edd2905fcc96
Marcus Cohee <82741496+marcuscohee@users.noreply.github.com>
2021-08-10 16:06:42 -0500

    Player and Laser Code Clean Up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 3.5f;
     [SerializeField] private GameObject _laserPrefab;
-
     [SerializeField] private float _fireRate = 0.15f;
     [SerializeField] private float _canFire = -1f;
-
-
-
-
 
     void Start()
     {
@@ -23,12 +18,12 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
-
-        if(Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
-            _canFire = Time.time + _fireRate;
-            Instantiate(_laserPrefab, transform.position + (Vector3.up * 0.8f), Quaternion.identity);
+            FireLaser();
         }
+
+
 
     }
     void CalculateMovement()
@@ -42,7 +37,7 @@ public class Player : MonoBehaviour
         //vertical player bounds
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
-        //horizontal player bounds
+        //horizontal player wrapping
         if (transform.position.x >= 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
@@ -51,6 +46,11 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
+    }
+    void FireLaser()
+    {
+      _canFire = Time.time + _fireRate;
+      Instantiate(_laserPrefab, transform.position + (Vector3.up * 0.8f), Quaternion.identity);
     }
 
 }
