b898133f3d5ab33917981aca31f8a129f8161ac7
Marcus Cohee <82741496+marcuscohee@users.noreply.github.com>
2021-08-18 23:21:51 -0500

    Displaying Iives on UI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -101,6 +101,8 @@ public class Player : MonoBehaviour
 
         _lives -= 1;
 
+        _uiManager.UpdateLives(_lives);
+
         if(_lives < 1)
         {
             _spawnManager.OnPlayerDeath();
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,14 +6,22 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private Text _scoreText;
+    [SerializeField] private Sprite[] _livesSprites;
+    [SerializeField] private Image _livesImg;
     void Start()
     {
         _scoreText.text = "Score: " + 0;
+        
     }
 
     public void UpdateScore(int playerScore)
     {
         _scoreText.text = "Score: " + playerScore;
     }
-    
+
+    public void UpdateLives(int currentLives)
+    {
+        _livesImg.sprite = _livesSprites[currentLives];
+    }
+
 }
