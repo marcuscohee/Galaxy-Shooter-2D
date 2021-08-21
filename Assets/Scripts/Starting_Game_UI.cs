2f116c35aecf6d5c88b176da8cf17798090186de
Marcus Cohee <82741496+marcuscohee@users.noreply.github.com>
2021-08-20 23:04:27 -0500

    GAME OVER UI and Restarting function

---
+++ Assets/Scripts/Starting_Game_UI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Starting_Game_UI : MonoBehaviour
+{
+    [SerializeField] private Text _countdownText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _countdownText.gameObject.SetActive(false);
+        StartCoroutine(CountdownTextRoutine());
+    }
+
+    IEnumerator CountdownTextRoutine()
+    {
+        yield return new WaitForSeconds(1.0f);
+        _countdownText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        _countdownText.text = "2";
+        yield return new WaitForSeconds(1.0f);
+        _countdownText.text = "1";
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene("Game");
+    }
+
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private Text _scoreText;
     [SerializeField] private Sprite[] _livesSprites;
     [SerializeField] private Image _livesImg;
+    [SerializeField] private Text _gameOverText;
+    [SerializeField] private Text _restartText;
+    [SerializeField] private bool _canRestart = false;
     void Start()
     {
         _scoreText.text = "Score: " + 0;
-        
+        _gameOverText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (_canRestart == true && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Starting_Scene");
+        }
     }
 
     public void UpdateScore(int playerScore)
@@ -22,6 +34,29 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _livesImg.sprite = _livesSprites[currentLives];
+
+        if(currentLives == 0)
+        {
+            
+            _gameOverText.gameObject.SetActive(true);
+            _restartText.gameObject.SetActive(true);
+            StartCoroutine(GameOverFlickerRoutine());
+            _canRestart = true;
+        }
+
     }
 
+    IEnumerator GameOverFlickerRoutine()
+
+    {
+        while (true)
+        {
+            _gameOverText.enabled = false;
+            _restartText.enabled = false;
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.enabled = true;
+            _restartText.enabled = true;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
 }
